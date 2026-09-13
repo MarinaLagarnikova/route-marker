e5665e64fa04630eceb809e05b70f1d69969a326
MarinaLagarnikova <m.lagarnikova@dvizh.io>
2026-09-13 12:41:04 +0300

    build: use postcss-preset-env for older WebView compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
-    autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 0,
+      browsers: 'chrome >= 91',
+      autoprefixer: true,
+      features: {
+        'cascade-layers': true,
+        'oklab-function': true,
+        'color-mix': true,
+      },
+    },
   },
 }
